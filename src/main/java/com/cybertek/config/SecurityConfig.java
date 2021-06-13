bb674484acee0f5ec075b289f81dd08d8991fe6f
MrtYvz192 <wingsoverthesky@gmail.com>
2021-06-13 18:29:00 +0300

    "Ticketing Project - Security"

--- src/main/java/com/cybertek/config/SecurityConfig.java
+++ src/main/java/com/cybertek/config/SecurityConfig.java
@@ -13,9 +13,11 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private SecurityService securityService;
+    private AuthSuccessHandler authSuccessHandler;
 
-    public SecurityConfig(SecurityService securityService) {
+    public SecurityConfig(SecurityService securityService, AuthSuccessHandler authSuccessHandler) {
         this.securityService = securityService;
+        this.authSuccessHandler = authSuccessHandler;
     }
 
     @Override
@@ -26,7 +28,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .formLogin()
                 .loginPage("/login")
-                .defaultSuccessUrl("/welcome")
+                //.defaultSuccessUrl("/welcome") --> not necessary when successHandler is used
+                .successHandler(authSuccessHandler)
                 .failureUrl("/login?error=true").permitAll()
                 .and()
                 .logout()
